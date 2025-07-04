0dd46f44004abd8a0d6dee2fe46733697d0218f5
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-07-05 01:50:28 +0530

    Added Heatmap for Players

--- app.py
+++ app.py
@@ -1,25 +1,64 @@
 import streamlit as st
 import pandas as pd
-from player_img import getPlayerImage
+from player_img_info import getPlayerImage, getPlayerInfo
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mplsoccer import Pitch
+
+def playerHeatMap(data,playerSelect):
+    heatData=data[(data['player']==playerSelect) & 
+                  (data['x']<=100) & 
+                  (data['y']<=100) & 
+                  (data['x']>0) & 
+                  (data['y']>0)]
+    
+    pitch = Pitch(pitch_type='opta', pitch_color='grass', line_color='black')
+    fig,ax= pitch.draw(figsize=(12,10))
+    
+    pitch.kdeplot(
+    heatData['x'],
+    heatData['y'],
+    fill=True,
+    cmap="Reds",
+    alpha=0.8,
+    ax=ax
+    )
+
+    st.pyplot(fig)
 
 teamsDF=pd.read_csv('data/PL_teams.csv')
 playersDF=pd.read_csv('data/Player.csv')
+data=pd.read_parquet('data\ENG_match_events.parquet')
+
 with st.sidebar:
     
     st.title('Premier League Player Analysis')
 
     teamSelect=st.selectbox("Select Team",teamsDF['team_name'])
+    
     players=playersDF[playersDF['Team']==teamSelect]
+    
     playerSelect=st.selectbox("Select Player",players['Player'].unique())
+    
     logo_path = teamsDF[teamsDF["team_name"] == teamSelect]["logo_url"].values[0]
+    
     st.image(logo_path)
     analyse=st.button("Analysis")
 
 playerInfo=playersDF[playersDF['Player']==playerSelect].iloc[0]
 
 st.title('Player Analysis 24/25 Season')
+
+imgURL=getPlayerImage(playerSelect)
+info=getPlayerInfo(playerSelect,teamSelect)
+
 if analyse:
     st.title(playerSelect)
-    imgURL=getPlayerImage(playerSelect, teamSelect)
     if imgURL:
         st.image(imgURL,width=250)
+    else:
+        st.write("😔 Can't display the fifa card becuase the free api can handle only 100 requests/day")
+    if info:
+        st.write(info)
+    st.title(f"{playerSelect} Heat Map")
+    playerHeatMap(data,playerSelect)
---
+++ player_img_info.py
@@ -0,0 +1,45 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_CUSTOM_SEARCH_API_KEY")
+GOOGLE_CX=os.getenv("GOOGLE_CX_API_KEY")
+
+def getPlayerImage(playerName):
+    query=f"{playerName} site:ea.com/games/ea-sports-fc/ratings"
+    url=f"https://www.googleapis.com/customsearch/v1"
+    param={
+        "key":GOOGLE_API_KEY,
+        "cx":GOOGLE_CX,
+        "q":query,
+        "searchType":"image",
+        "num":1
+    }
+    try:
+        response=requests.get(url,params=param)
+        data=response.json()
+        if "items" in data:
+            return data['items'][0]['link']
+    
+    except Exception as e:
+        return e
+
+def getPlayerInfo(playerName,teamName):
+    query=f"{playerName} {teamName} wikipedia"
+    url=f"https://www.googleapis.com/customsearch/v1"
+    param={
+        "key":GOOGLE_API_KEY,
+        "cx":GOOGLE_CX,
+        "q":query
+    }
+    
+    try:
+        response=requests.get(url,param)
+        data=response.json()
+        if "items" in data:
+            return data['items'][0]['snippet']
+    
+    except Exception as e:
+        return e
